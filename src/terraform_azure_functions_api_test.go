182a4b90efbeebf8c9e92e789b706e1f22e873be
Lee Beenen <leebeenen@gmail.com>
2021-09-16 10:34:19 +0200

    Add a new test, posting a new item and expecting the title of said item back

--- src/terraform_azure_functions_api_test.go
+++ src/terraform_azure_functions_api_test.go
@@ -24,8 +24,13 @@ func TestAzureFunctionsAPIReachable(t *testing.T) {
 	api_url := terraform.Output(t, terraformOptions, "api_url")
 
 	url := fmt.Sprintf("https://%s/api/news", api_url)
-	http_helper.HttpGetWithRetry(t, url, nil, 200, "[]", 30, 5*time.Second)
+	http_helper.HttpGetWithRetry(t, url, nil, 200, "[]", 30, 5*time.Second) //As we've just initialized the environment, calling the url with all the news should result in an empty response body.
 
 	failurl := fmt.Sprintf("https://%s/api/news/this is a test", api_url)
-	http_helper.HttpGetWithRetry(t, failurl, nil, 404, "", 30, 5*time.Second)	
+	http_helper.HttpGetWithRetry(t, failurl, nil, 404, "", 30, 5*time.Second) //By the same logic, retrieving a specific title "this is a test" should result in a 404 being returned.
+
+	posturl := fmt.Sprintf("https://%s/api/newsitem", api_url)
+	body := []byte("{\"Title\": \"This is a test.\", \"Description\": \"It's for testing!\"}")
+	headers := map[string]string{}
+	http_helper.HTTPDoWithValidationRetry(t, "POST", posturl, body, headers, 200, "Your article with title 'This is a test.' was saved successfully.", 30, 5*time.Second, nil)
 }
